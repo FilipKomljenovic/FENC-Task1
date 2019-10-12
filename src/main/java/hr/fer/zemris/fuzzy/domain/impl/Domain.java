b18bfc70c1edae9a14f02e798231d20ade948c04
Filip Komljenovic <filip.komljenovic@fer.hr>
2019-10-12 22:09:11 +0200

    fix combine method

--- src/main/java/hr/fer/zemris/fuzzy/domain/impl/Domain.java
+++ src/main/java/hr/fer/zemris/fuzzy/domain/impl/Domain.java
@@ -15,10 +15,18 @@ public abstract class Domain implements IDomain {
 
     public static IDomain combine(IDomain d1, IDomain d2) {
         List<SimpleDomain> domainsList = new ArrayList<>();
-        domainsList.add((SimpleDomain) d1);
-        domainsList.add((SimpleDomain) d2);
-        SimpleDomain[] domains=new SimpleDomain[domainsList.size()];
+        domainsList.addAll(extractSimpleDomainsFromIDomain(d1));
+        domainsList.addAll(extractSimpleDomainsFromIDomain(d2));
+        SimpleDomain[] domains = new SimpleDomain[domainsList.size()];
         return new CompositeDomain(domainsList.toArray(domains));
+    }
+
+    private static List<SimpleDomain> extractSimpleDomainsFromIDomain(IDomain d1) {
+        List<SimpleDomain> simpleDomains = new ArrayList<>();
+        for (int i = 0; i < d1.getNumberOfComponents(); i++) {
+            simpleDomains.add((SimpleDomain) d1.getComponent(i));
+        }
+        return simpleDomains;
     }
 
     @Override
